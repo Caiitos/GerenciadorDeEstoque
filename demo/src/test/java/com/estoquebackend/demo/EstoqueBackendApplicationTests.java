852f1a75c6596e1415a5f50e23b929f3b8345439
Caio Melo <caiopedrosa2003@gmail.com>
2026-03-07 08:09:31 -0300

    Adicionando Spring Boot

---
+++ demo/src/test/java/com/estoquebackend/demo/EstoqueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estoquebackend.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
